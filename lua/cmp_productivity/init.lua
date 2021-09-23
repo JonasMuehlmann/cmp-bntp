2fd84a7ef2fd1bd8e26635ee14cf88dc9e46321a
jonas <jonasmuehlmann@protonmail.com>
2021-09-23 18:47:27 +0200

    chore:cleanup

--- lua/cmp_productivity/init.lua
+++ lua/cmp_productivity/init.lua
@@ -7,38 +7,16 @@ source.new = function()
   return self
 end
 
------Return the source is available or not.
------@return boolean
---function source:is_available()
---  return true
---end
+-- ---Return the source is available or not.
+-- ---@return boolean
+-- function source:is_available()
+--   return true
+-- end
 
------Return the source name for some information.
---function source:get_debug_name()
---  return 'example'
---end
-
------Return keyword pattern which will be used...
------  1. Trigger keyword completion
------  2. Detect menu start offset
------  3. Reset completion state
------@param params cmp.SourceBaseApiParams
------@return string
---function source:get_keyword_pattern(params)
---  return '???'
---end
-
------Return trigger characters.
------@param params cmp.SourceBaseApiParams
------@return string[]
---function source:get_trigger_characters(params)
---  return { ??? }
---end
-
----Invoke completion (required).
----  If you want to abort completion, just call the callback without arguments.
----@param params cmp.SourceCompletionApiParams
----@param callback fun(response: lsp.CompletionResponse|nil)
+-- Invoke completion (required).
+--   If you want to abort completion, just call the callback without arguments.
+-- @param params cmp.SourceCompletionApiParams
+-- @param callback fun(response: lsp.CompletionResponse|nil)
 function source:complete(params, callback)
   callback({
     { label = 'January' },
@@ -56,19 +34,4 @@ function source:complete(params, callback)
   })
 end
 
------Resolve completion item that will be called when the item selected or before the item confirmation.
------@param completion_item lsp.CompletionItem
------@param callback fun(completion_item: lsp.CompletionItem|nil)
---function source:resolve(completion_item, callback)
---  callback(completion_item)
---end
-
------Execute command that will be called when after the item confirmation.
------@param completion_item lsp.CompletionItem
------@param callback fun(completion_item: lsp.CompletionItem|nil)
---function source:execute(completion_item, callback)
---  callback(completion_item)
---end
-
 return source
--- print("Hello World")
